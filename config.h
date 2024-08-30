c8a819608e76eb91553dcb088e36f067ad4fe997
Ryan @ ArchDesktop <ryan@archdesktop>
2024-08-30 16:11:27 -0500

    add systray

--- config.h
+++ config.h
@@ -3,16 +3,21 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static unsigned int borderpx = 1; /* border pixel of windows */
-static unsigned int snap = 32;	  /* snap pixel */
-static unsigned int gappih = 2;	  /* horiz inner gap between windows */
-static unsigned int gappiv = 2;	  /* vert inner gap between windows */
-static unsigned int gappoh = 0;	  /* horiz outer gap between windows and screen edge */
-static unsigned int gappov = 0;	  /* vert outer gap between windows and screen edge */
-static int swallowfloating = 0;	  /* 1 means swallow floating windows by default */
-static int smartgaps = 1;		  /* 1 means no outer gap when there is only one window */
-static int showbar = 1;			  /* 0 means no bar */
-static int topbar = 1;			  /* 0 means bottom bar */
+static unsigned int borderpx = 1;			  /* border pixel of windows */
+static unsigned int snap = 32;				  /* snap pixel */
+static unsigned int gappih = 2;				  /* horiz inner gap between windows */
+static unsigned int gappiv = 2;				  /* vert inner gap between windows */
+static unsigned int gappoh = 0;				  /* horiz outer gap between windows and screen edge */
+static unsigned int gappov = 0;				  /* vert outer gap between windows and screen edge */
+static int swallowfloating = 0;				  /* 1 means swallow floating windows by default */
+static int smartgaps = 1;					  /* 1 means no outer gap when there is only one window */
+static const unsigned int systraypinning = 0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;  /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2; /* systray spacing */
+static const int systraypinningfailfirst = 1; /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray = 1;			  /* 0 means no systray */
+static int showbar = 1;						  /* 0 means no bar */
+static int topbar = 1;						  /* 0 means bottom bar */
 static char *fonts[] = {"HackNerdFont:size=12:antialias=true"};
 static char normbgcolor[] = "#222222";
 static char normbordercolor[] = "#ffffff";
