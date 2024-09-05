a59e3ff4dbce4309619a938dfe200482bf74da35
Ryan @ ArchDesktop <ryan@archdesktop>
2024-09-05 11:32:10 -0500

    remove gaps with one window

--- config.h
+++ config.h
@@ -3,10 +3,10 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static unsigned int borderpx = 1;			  /* border pixel of windows */
+static unsigned int borderpx = 0;			  /* border pixel of windows */
 static unsigned int snap = 32;				  /* snap pixel */
-static unsigned int gappih = 2;				  /* horiz inner gap between windows */
-static unsigned int gappiv = 2;				  /* vert inner gap between windows */
+static unsigned int gappih = 3;				  /* horiz inner gap between windows */
+static unsigned int gappiv = 3;				  /* vert inner gap between windows */
 static unsigned int gappoh = 0;				  /* horiz outer gap between windows and screen edge */
 static unsigned int gappov = 0;				  /* vert outer gap between windows and screen edge */
 static int swallowfloating = 0;				  /* 1 means swallow floating windows by default */
